f331630eb5d001b2a4c8e864c10d5c143ad7bb51
Carl Llemos <iamcarlllemos@gmail.com>
2025-10-14 08:03:35 +0800

    latest

--- database/migrations/2025_10_01_100002_create_application_approvers.php
+++ database/migrations/2025_10_01_100002_create_application_approvers.php
@@ -26,7 +26,24 @@ return new class extends Migration
         });
 
         Schema::create('applications_approvals', function(Blueprint $table) {
-           
+            $table->id();
+            $table->foreignId('application_id')
+                ->constrained('applications')
+                ->onDelete('cascade');
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->onDelete('cascade');
+            $table->enum('status', [
+                'cancelled',
+                'pending',
+                'approved',
+                'rejected'
+            ])->default('pending');
+            $table->longText('remarks')
+                ->nullable();
+            $table->timestamp('action_at')
+                ->nullable();
+            $table->timestamps();
         });
     }
 
