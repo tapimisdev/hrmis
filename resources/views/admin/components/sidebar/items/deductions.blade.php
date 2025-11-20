1daf72117f6db9d7e88a9a9d6da7c2336db12e0f
KemuelJoshua <kemueljoshuamariano@gmail.com>
2025-11-20 13:20:49 +0800

    deduction sub module

--- resources/views/admin/components/sidebar/items/deductions.blade.php
+++ resources/views/admin/components/sidebar/items/deductions.blade.php
@@ -14,22 +14,50 @@
             <li class="nested-item">
                 <a href="{{ route('tax.salary.index') }}"
                 class="{{ request()->routeIs('tax.salary.index') ? 'active' : '' }}">
-                    <i class="fa-solid fa-receipt"></i>
+                    <i class="fa-solid fa-percent"></i>
                     <span>Salary Tax</span>
                 </a>
             </li>
             <li class="nested-item">
                 <a href="{{ route('salary.index') }}"
                 class="{{ request()->routeIs('salary.index') ? 'active' : '' }}">
-                    <i class="fa-solid fa-file-lines"></i>
+                    <i class="fa-solid fa-home"></i>
                     <span>HP Tax</span>
                 </a>
             </li>
             <li class="nested-item">
                 <a href="{{ route('salary.index') }}"
                 class="{{ request()->routeIs('salary.index') ? 'active' : '' }}">
-                    <i class="fa-solid fa-file-contract"></i>
+                    <i class="fa-solid fa-clock"></i>
                     <span>Longe-tax</span>
+                </a>
+            </li>
+            <li class="nested-item">
+                <a href="{{ route('salary.index') }}"
+                class="{{ request()->routeIs('salary.index') ? 'active' : '' }}">
+                    <i class="fa-solid fa-hand-holding-heart"></i>
+                    <span>Pagibig</span>
+                </a>
+            </li>
+            <li class="nested-item">
+                <a href="{{ route('salary.index') }}"
+                class="{{ request()->routeIs('salary.index') ? 'active' : '' }}">
+                    <i class="fa-solid fa-heart-pulse"></i>
+                    <span>Philhealth</span>
+                </a>
+            </li>
+            <li class="nested-item">
+                <a href="{{ route('salary.index') }}"
+                class="{{ request()->routeIs('salary.index') ? 'active' : '' }}">
+                    <i class="fa-solid fa-shield"></i>
+                    <span>GSIS</span>
+                </a>
+            </li>
+            <li class="nested-item">
+                <a href="{{ route('salary.index') }}"
+                class="{{ request()->routeIs('salary.index') ? 'active' : '' }}">
+                    <i class="fa-solid fa-university"></i>
+                    <span>Landbank</span>
                 </a>
             </li>
         </ul>
