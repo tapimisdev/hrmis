743bfb4d9f73302ac0ecc0771dd1d8ff94c8f770
KemuelJoshua <kemueljoshuamariano@gmail.com>
2025-12-19 09:22:16 +0800

    dashboard change

--- routes/apis/dashboard.php
+++ routes/apis/dashboard.php
@@ -6,8 +6,4 @@ use App\Http\Controllers\Api\DashboardApiController;
  # PREFIX
 Route::prefix('dashboard')->group(function() {
     Route::get('metrics', [DashboardApiController::class, 'metrics']);
-    Route::get('birthdays', [DashboardApiController::class, 'birthdays']);
-    Route::get('attendances', [DashboardApiController::class, 'attendances']);
-    Route::get('employment-types', [DashboardApiController::class, 'employment_types']);
-    Route::get('employee-movement', [DashboardApiController::class, 'employee_movement']);
 });
