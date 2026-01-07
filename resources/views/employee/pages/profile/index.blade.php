46f55c67143f6d6f482b856eb7ff85c13a90e387
KemuelJoshua <kemueljoshuamariano@gmail.com>
2026-01-07 15:12:34 +0800

    description changed

--- resources/views/employee/pages/profile/index.blade.php
+++ resources/views/employee/pages/profile/index.blade.php
@@ -7,7 +7,7 @@
         <header-vue title="DOST TAPI"></header-vue>
     </x-employee-navbar>
         
-    <x-header-employee title="Profile" subtitle="Profile ko to ha!">
+    <x-header-employee title="Profile" subtitle="View and manage your information">
 
     </x-header-employee>
 
