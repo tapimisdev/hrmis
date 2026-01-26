5ad30a104b28e94d927a3bff089d0950e904e88b
KemuelJoshua <kemueljoshuamariano@gmail.com>
2026-01-26 12:50:33 +0800

    embed to tutorial

--- resources/js/employee/components/parts/Tutorials.vue
+++ resources/js/employee/components/parts/Tutorials.vue
@@ -22,7 +22,14 @@
             </div>
         </div>
         <div class="widget-content">
-
+            <iframe
+                src="https://player.vimeo.com/video/1158227042?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+                frameborder="0"
+                allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"
+                referrerpolicy="strict-origin-when-cross-origin"
+                style="position:absolute;top:0;left:0;width:100%;height:100%;"
+                title="ORBIT ESS">
+            </iframe>
         </div>
     </div>
 </template>
