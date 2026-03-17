756b6349eccfafe2bdc9c650ff12af1413775fcb
Carl Llemos <iamcarlllemos@gmail.com>
2026-03-17 10:50:31 +0800

    fixed online users

--- resources/js/laravel-echo.js
+++ resources/js/laravel-echo.js
@@ -7,7 +7,7 @@ window.Echo = new Echo({
   broadcaster: 'reverb',
   key: import.meta.env.VITE_REVERB_APP_KEY,
 
-  wsHost: import.meta.env.VITE_REVERB_HOST ?? window.location.hostname,
+  wsHost: window.location.hostname,
   wsPort: Number(import.meta.env.VITE_REVERB_PORT ?? 8081),
   wssPort: Number(import.meta.env.VITE_REVERB_PORT ?? 8081),
 
