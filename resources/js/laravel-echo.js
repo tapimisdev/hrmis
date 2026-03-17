a031d221cdeee4c55e3a100d17c35bab0259d443
Carl Llemos <iamcarlllemos@gmail.com>
2026-03-17 10:32:44 +0800

    fixed online users

--- resources/js/laravel-echo.js
+++ resources/js/laravel-echo.js
@@ -3,16 +3,33 @@ import Pusher from "pusher-js";
 
 window.Pusher = Pusher;
 
+// window.Echo = new Echo({
+//     broadcaster: 'reverb',
+//     key: import.meta.env.VITE_REVERB_APP_KEY,
+
+//     wsHost: import.meta.env.VITE_REVERB_HOST || window.location.hostname,
+
+//     wsPort: Number(import.meta.env.VITE_REVERB_PORT || 8081),
+//     wssPort: Number(import.meta.env.VITE_REVERB_PORT || 443),
+
+//     forceTLS: window.location.protocol === 'https:',
+
+//     enabledTransports: ['ws', 'wss'],
+
+//     authEndpoint: '/broadcasting/auth',
+//     withCredentials: true,
+// });
+
 window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
 
-    wsHost: import.meta.env.VITE_REVERB_HOST || window.location.hostname,
+    wsHost: window.location.hostname,
 
-    wsPort: Number(import.meta.env.VITE_REVERB_PORT || 8081),
-    wssPort: Number(import.meta.env.VITE_REVERB_PORT || 443),
+    wsPort: 80,
+    wssPort: 443,
 
-    forceTLS: window.location.protocol === 'https:',
+    forceTLS: true,
 
     enabledTransports: ['ws', 'wss'],
 
