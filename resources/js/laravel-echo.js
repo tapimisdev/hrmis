f092efb8afd7945c47220ded72e62d50238d2135
Carl Llemos <iamcarlllemos@gmail.com>
2026-03-26 15:13:43 +0800

    test

--- resources/js/laravel-echo.js
+++ resources/js/laravel-echo.js
@@ -5,6 +5,7 @@ window.Pusher = Pusher;
 
 window.Echo = new Echo({
   broadcaster: 'reverb',
+  
   key: import.meta.env.VITE_REVERB_APP_KEY,
 
   wsHost: import.meta.env.VITE_REVERB_HOST ?? window.location.hostname,
